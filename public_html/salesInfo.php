4686713f78ce9d7b48015c8991b25f4ac3c0047b
Robert Hicks <robert@ip-172-31-32-198.us-east-2.compute.internal>
2019-04-15 21:26:17 +0000

    view sales history for each item

---
+++ public_html/salesInfo.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport content="width=device-width, initial-scale=1.0">
+		<link href="styles/resultsstyle.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"> 
+        <title>Results</title>
+        <?PHP include 'nav.php'; ?>
+    </head>
+
+
+<?php 
+  include 'fun.php';
+include 'user.php';
+    $query = "select * from sales where item = '" .$_POST["test"] ."' and store=1;";
+   echo "<h1> Sales History <h1>";
+   $sqlquery=$conn->prepare($query);
+   $sqlquery->execute();
+ 
+    $i=0;
+    for($i = 0; $row = $sqlquery->fetch(); $i++)
+   {
+        $items [$i] = array('UPC'=>$row[0], 'Store'=>$row[1], 'Quantity'=>$row[2], 'Time of Sale'=>$row[3], 'Sale ID'=>$row[4]);
+    }
+
+    
+    
+    if ($i == 0)
+      echo"<h1>No sales history found.</h1>";
+    echo build_table($items);
+?>
+
+</html>
--- public_html/search.php
+++ public_html/search.php
@@ -37,10 +37,75 @@
     }
 
     
-    echo "<h1> Results <h1>";
+    echo "<h1> Results </h1>";
     if ($i == 0)
       echo"<h1>No results found for '" .$_POST["itemName"] ."'</h1>";
+    echo "<form id=\"resultsForm\" method=\"post\">";
     echo build_table($items); //html table
+    echo "</form>";
 ?>
     </body>
+    <script>
+ /*   function background(){
+    	var background = document.createElement('div');
+    	background.setAttribute('class', 'background');
+    	background.setAttribute("width",window.innerHeight);
+    	background.setAttribute("height",window.innerWidth);
+    	
+    	return background;
+    }*/
+    function submitForm(action){
+	    document.getElementById('resultsForm').action = action;
+    	    document.getElementById('resultsForm').submit();	    
+    }
+      var rows = document.querySelectorAll("tr");
+      for (var i = 1; i < rows.length; i++) {
+        var moreInfoButton = document.createElement("a");
+	//moreInfoButton.setAttribute("type", "submit");
+	moreInfoButton.href="#";
+	moreInfoButton.innerText="[More Info]";
+        //moreInfoButton.onclick=function(popup){
+        /*var popup = document.createElement("div");
+        popup.setAttribute("class", "moreInfo");
+          var upc = this.parentNode.children[0].innerText;
+          console.log(upc);
+          var b = document.querySelector("body");
+          b.appendChild(background());
+          
+          b.appendChild(popup);
+          var exitButton = document.createElement('button');
+      		exitButton.setAttribute('class', 'exitInfo');
+      		exitButton.innerText='x';
+      		exitButton.onclick=function(){
+            var moreInfo = document.querySelector('.moreInfo');
+      			popup.remove();
+      			var bg = document.querySelector('.background');
+      			bg.remove();
+      		};
+          
+      		popup.prepend(exitButton);*/
+          
+	//};
+	//
+	moreInfoButton.onclick=function(){
+		var upc = this.parentNode.children[0].innerText;
+		//upcContainer.setAttribute("name", "selected");
+		//upcContainer.setAttribute("value", upcContainer.innerText);
+		console.log(upc);
+		var t = document.createElement("input");
+		t.setAttribute("name", "test");
+		t.setAttribute("value", upc);
+		var c=this;
+		for(var i = 0; i < 4; i++) 
+			c=c.parentNode;
+		console.log(c);
+		c.appendChild(t);
+		submitForm('salesInfo.php');
+	};
+        rows[i].appendChild(moreInfoButton);
+        
+      }
+    
+
+    </script>
 </html>
