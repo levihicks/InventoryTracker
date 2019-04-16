73d8a8ba0ba85c683ffbb916db6a97eae5bf4f7c
Alec Straw <47575949+akstraw@users.noreply.github.com>
2019-04-16 14:12:25 -0500

    add user

--- public_html/adduser.php
+++ public_html/adduser.php
@@ -1,4 +1,4 @@
-<?PHP include 'user.php'; ?>
+<?PHP include 'user.php'; include 'fun.php' ?>
 <!DOCTYPE html>
 <html>
     <head>   <title>Staff</title>
@@ -8,17 +8,58 @@
 		<?PHP include 'nav.php'; ?>
 	</head>
         <body>
-          
+          <?PHP if ( !isset($_POST["submit"]) ) : ?>
             <form action="adduser.php" method="post">
-                Enter Position<input type="textbox" name="Position" ><br>
-                Enter Name<input type="textbox" name="name" ><br>
-                Enter Username<input type="textbox" name="uname" ><br>
-                Enter Password<input type="password" name="password" ><br>
+                Position<input type="textbox" name="Position" ><br>
+                Name<input type="textbox" name="Name" ><br>
+                Username<input type="textbox" name="Uname" ><br>
+                Password<input type="password" name="Password" ><br>
                 Store<select name="Store">
-                    <option>1</option>
+                    <option value="">Select...</option>
+                <?PHP
+                //store options
+                    $store_numbers = query_1D_array("SELECT num FROM stores", $conn);
+                    foreach($store_numbers as $option){
+                        echo ' <option value="' . $option . '">';
+                        echo $option . "</option>\r\n";
+                    }
+                ?>
                 </select><br>
-                <input type="submit" value="Add User"><br>
+                <input type="submit" value="Submit" name="submit"><br><br>
             </form>
+            <?PHP
+                else:
+                $attributes = array("Position", "Name", "Uname","Password", "Store");
+                //check valuse are set
+                $missing = false;
+                foreach($attributes as $key)
+                {
+                    if ( isset($_POST[$key]) && ( strlen($_POST[$key]) > 1 || $key == "Store" ) )
+                        $attributes[$key] = $_POST[$key];
+                    else{
+                        echo "<p> $key is required";
+                        $missing = true;
+                    }
+                }
+                //if none missing then try to add user
+                if ($missing !== true){
+                    try{
+                        $query = "INSERT INTO employees VALUES (?, ?, ?, ?)";
+                        $sqlquery=$conn->prepare($query);
+                        $sqlquery->execute([ $_POST["Uname"], $_POST["Name"],
+                                            $_POST["Position"], $_POST["Password"] ]);
+                        $query = "INSERT INTO employs VALUES (?, ?);";
+                        $sqlquery=$conn->prepare($query);
+                        $sqlquery->execute([ $_POST["Store"], $_POST["Uname"] ]);
+                        echo "<p> New user added.</p>";
+                    }
+                    catch (PDOException $e){
+                        echo "Query Invalid: " .$e;
+                    }
+                    
+                }
+            ?>
+            <?PHP endif; ?>
             <form action="staff.php" method="post">
                     <input type="submit" value="Back">
             </form>
--- public_html/staff.php
+++ public_html/staff.php
@@ -8,12 +8,28 @@
 		<?PHP include 'nav.php'; ?>
 	</head>
         <body>
-          <p>Select Store</p>
-                <?php
-                        include "staffLogic.php";
-                ?>
-            <form action="adduser.php" method="post">
-                <input type="submit" value="Add User">
-            </form>
+          
+            <?php if( isset($_POST["removeSubmit"]) ) /* Remove User form */:?>
+                <br>
+                <form action="staff.php" method="post">
+                    Username<input type="textbox" name="removedUser">
+                    <input type="submit" value="Submit" name="removeSubmit2">
+                </form>
+            <?php elseif ( isset($_POST["removeSubmit2"]) ) :
+                echo "<p> Submitted </p>";
+            ?>
+            <?php else :  //if not removing user
+                    echo "<p>Select Store</p>";
+                    include "staffLogic.php";
+            ?>
+                <br>
+                <form action="adduser.php" method="post">
+                    <input type="submit" value="Add User">
+                </form>
+                <form action="staff.php" method="post">
+                    <input type="submit" value="Remove User" name="removeSubmit">
+                </form>
+            <?php endif; ?>
+            
         </body>
 </html>
